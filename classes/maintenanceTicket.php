863edb7588ccab0606bd6dbbcc63a581004ae631
EngAsmaaRamadan <asmdrsnjd@gmail.com>
2026-05-05 22:31:11 +0300

    getterand setter and access modifier and constructor

--- classes/maintenanceTicket.php
+++ classes/maintenanceTicket.php
@@ -1,19 +1,60 @@
 <?php
 class MaintenanceTicket {
-    public string $ticketId;
-    public string $issueDescription;
-    public string $status;
-    
-    
+    private string $ticketId;
+    private string $issueDescription;
+    private string $status;
+    private Tool $tool;
+    private DateTime $createdAtDate;
     public function __construct(string $ticketId, Tool $tool, string $issueDescription) {
         $this->ticketId = $ticketId;
         $this->tool = $tool;
         $this->issueDescription = $issueDescription;
         $this->status = "Open";
-        $this->createdAt = new DateTime();
+        $this->createdAtDate = new DateTime();
+    }
+        public function getTicketId(): string {
+        return $this->ticketId;
     }
     
-    public function closeTicket();
+    public function setTicketId(string $ticketId): void {
+        $this->ticketId = $ticketId;
+    }
     
-  
-}
+    public function getIssueDescription(): string {
+        return $this->issueDescription;
+    }
+    
+    public function setIssueDescription(string $issueDescription): void {
+        $this->issueDescription = $issueDescription;
+    }
+    
+    public function getStatus(): string {
+        return $this->status;
+    }
+    
+    public function setStatus(string $status): void {
+        $this->status = $status;
+    }
+    
+    public function getTool(): Tool {
+        return $this->tool;
+    }
+    
+    public function setTool(Tool $tool): void {
+        $this->tool = $tool;
+    }
+    
+    public function getCreatedAtDate(): DateTime {
+        return $this->createdAtDate;
+    }
+    
+    public function setCreatedAtDate(DateTime $createdAtDate): void {
+        $this->createdAtDate = $createdAtDate;
+    }
+    
+    public function closeTicket(){
+        $this->status = "Closed";
+    }
+
+
+?>

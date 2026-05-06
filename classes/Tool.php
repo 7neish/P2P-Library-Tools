0d1c84713be92ede9638ecc7eb404088f6ddbbb3
yahya-249 <yahyamohamed2492006@gmail.com>
2026-05-06 22:54:01 +0300

    Add files via upload

---
+++ classes/Tool.php
@@ -0,0 +1,24 @@
+<?php
+class Tool {
+    public $tool_id;
+    public $owner_id;
+    public $category_id;
+    public $zone_id;
+    public $tool_name;
+    public $description;
+    public $serial_number;
+    public $hourly_rate;
+    public $daily_rate;
+    public $weekly_rate;
+    public $deposit_amount;
+    public $buffer_hours;
+    public $status;
+    public $latitude;
+    public $longitude;
+    public $created_at;
+    public $category_name;
+    public $owner_name;
+    public $owner_phone;
+    public $distance_km;
+    public $image_url;
+}
